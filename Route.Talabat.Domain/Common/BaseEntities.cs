91c793751b24f7676e865726c7810f5011a32f09
Eslam mohamed <em354219@gmail.com>
2024-10-07 07:17:43 +0300

    Part 02 BaseEntity

---
+++ Route.Talabat.Domain/Common/BaseEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Domain.Common
+{
+    public abstract class BaseEntities<TKey> where TKey : IEquatable<TKey>
+    {
+        public required  TKey Id { get; set; }
+
+        #region Administration field
+        public required string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public required string LastModifiedBy { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        #endregion
+
+    }
+}
