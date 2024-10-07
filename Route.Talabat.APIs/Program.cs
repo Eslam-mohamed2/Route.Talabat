d98d7ca84b9d601e20a6177c61006d7f0f692e5b
Eslam mohamed <em354219@gmail.com>
2024-10-07 06:41:59 +0300

    Seesion 01 Content

---
+++ Route.Talabat.APIs/Program.cs
@@ -0,0 +1,41 @@
+
+namespace Route.Talabat.APIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            #region Configure Services
+
+            webApplicationBuilder.Services.AddControllers(); // Register the Required Services
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            webApplicationBuilder.Services.AddEndpointsApiExplorer();
+            webApplicationBuilder.Services.AddSwaggerGen(); 
+
+            #endregion
+
+            var app = webApplicationBuilder.Build();
+
+            #region Configrue Kestrel Middlewares
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            #endregion
+
+            app.Run(); 
+        }
+    }
+}
