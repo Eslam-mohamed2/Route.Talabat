792ba5c8816021e93c8daa9c088462d3e30891e9
Eslam mohamed <em354219@gmail.com>
2024-10-16 00:42:45 +0300

    Part 12 Basket Service

--- Route.Talabat.Application/DependencyInjection.cs
+++ Route.Talabat.Application/DependencyInjection.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Route.Talaat.Core.Application.Abstraction.Services;
 using Route.Talaat.Core.Application.Abstraction.Services.Products;
 using Route.Talaat.Core.Application.Mapping;
 using Route.Talaat.Core.Application.Services;
 using Route.Talaat.Core.Application.Services.Products;
+using Route.Talabat.Core.Application.Abstraction.Services.Basket;
+using Route.Talabat.Core.Application.Services.Basket;
+using Route.Talabat.Core.Domain.Contracts.Infrastructure;
 
 namespace Route.Talaat.Core.Application
 {
@@ -17,6 +22,17 @@ namespace Route.Talaat.Core.Application
             services.AddScoped(typeof(IProductService), typeof (ProductService));
 
             services.AddScoped<IServiceManger, ServiceManger>();
+
+            //services.AddScoped(typeof(IBasketService), typeof(BasketService));
+            //services.AddScoped(typeof(Func<IBasketService>),typeof(Func<BasketService>))
+
+            services.AddScoped(typeof(Func<IBasketService>), (ServiceProvider) =>
+            {
+                var mapper = ServiceProvider.GetRequiredService<IMapper>();
+                var Configuration = ServiceProvider.GetRequiredService<IConfiguration>();
+                var basketRepository = ServiceProvider.GetRequiredService<IBasketRepository>();
+                return new BasketService(basketRepository,mapper, Configuration);   
+            });
 ;           return services;
         }
     }
--- Route.Talabat.Application/Services/ServiceManger.cs
+++ Route.Talabat.Application/Services/ServiceManger.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
 using Route.Talaat.Core.Application.Abstraction.Services;
 using Route.Talaat.Core.Application.Abstraction.Services.Products;
 using Route.Talaat.Core.Application.Services.Products;
+using Route.Talabat.Core.Application.Abstraction.Services.Basket;
 using Route.Talabat.Core.Application.Abstraction.Services.Employees;
+using Route.Talabat.Core.Application.Services.Basket;
 using Route.Talabat.Core.Application.Services.Employees;
+using Route.Talabat.Core.Domain.Contracts.Infrastructure;
 using Route.Talabat.Core.Domain.Contracts.Persistence;
 using System;
 using System.Collections.Generic;
@@ -17,20 +21,24 @@ namespace Route.Talaat.Core.Application.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-
+        private readonly IConfiguration _configuration;
         private readonly Lazy<IProductService> _productService;
         private readonly Lazy<IEmployeeService> _employeeService;
+        private readonly Lazy<IBasketService> _basketService;
 
-        public ServiceManger(IUnitOfWork unitOfWork, IMapper mapper)
+        public ServiceManger(IUnitOfWork unitOfWork, IMapper mapper,IConfiguration configuration,Func<IBasketService> basketServiceFactory)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _configuration = configuration;
             _productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
             _employeeService = new Lazy<IEmployeeService>(() => new EmployeeService(_unitOfWork, _mapper));
+            _basketService = new Lazy<IBasketService>(basketServiceFactory);
         }
 
         public IProductService ProductService => _productService.Value;
         public IEmployeeService EmployeeService => _employeeService.Value;
 
+        public IBasketService BasketService => throw new NotImplementedException();
     }
 }
