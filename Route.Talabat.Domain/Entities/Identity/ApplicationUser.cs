a1fc95aea1a5913b949fc3f9e29d336448a681e6
Eslam mohamed <em354219@gmail.com>
2024-10-17 19:59:42 +0300

    Part 01 Identity Module - Entities

---
+++ Route.Talabat.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Domain.Entities.Identity
+{
+        public class ApplicationUser : IdentityUser<string>  // There are 2 overlaods non Generic Option -> by Default the Id will be String 
+                                                             // the Second Option is the Generic Option 
+        {
+        private Address? address;
+
+        public required string DisplayName { get; set; }
+
+        public Address? Address { get => address; set => address = value; }
+    }
+}
